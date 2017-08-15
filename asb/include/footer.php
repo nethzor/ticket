c76833961b9012325729377b94cf03aae2b5cc4a
Unknown <xpretorius@gmail.com>
2017-08-15 17:28:24 +0200

    LOGIN
    LOGIN EXTERNAL

---
+++ asb/include/footer.php
@@ -0,0 +1,22 @@
+<!-- Include CSS -->
+<link href="./css/bootstrap.min.css" rel="stylesheet"><!-- Bootstrap CSS -->
+<link href="./css/style.css" rel="stylesheet"><!-- Custom CSS -->
+<!-- Include Google font -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,300,600">
+<!-- Include JavaScripts -->
+<script src="./js/jquery.min.js" async></script><!-- Load jquery -->
+<script src="./js/bootstrap.min.js" async></script><!-- Load bootstrap js -->
+<script src="./js/app.js" async></script><!-- Load custom js -->
+
+		<!--DIE CHAT SE LINK-->
+	<!--https://salesiq.zoho.com/-->	
+	<script type="text/javascript">
+	var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || 
+	{widgetcode:"7326515b74ea374c61d3ec46aa879f9d39ec90e79be88a4b11ad7ea2f28f1c40", values:{},ready:function(){}};
+	var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
+	s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+	</script>
+	<!--End van chat se link-->
+
+</body>
+</html>
